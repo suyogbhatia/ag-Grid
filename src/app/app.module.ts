fe5ee5dd1526490ed9530faa1c79b1e61e044f33
Suyog Bhatia <suyog_bhatia@apple.com>
2020-03-02 17:29:55 +0530

    cell style rgb and coordinates given

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SampleGridComponent } from './sample-grid/sample-grid.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleGridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
